83785ce39441b8e59ba622b223dda75a085bd51c
Martin Pfaller <martin.pfaller@yale.edu>
2025-03-12 13:34:40 -0400

    rough first RC estimation

--- process.py
+++ process.py
@@ -9,6 +9,7 @@ import numpy as np
 import json
 from scipy.interpolate import interp1d
 from scipy.optimize import minimize, least_squares, Bounds
+from scipy.signal import savgol_filter
 
 
 def read_data(csv_file):
@@ -28,42 +29,11 @@ def create_forward_config(t, pa, intype):
     elif intype == "q":
         bc_type = "FLOW"
         bc_val = "Q"
-    forward = {
-        "boundary_conditions": [
-            {
-                "bc_name": "pa",
-                "bc_type": bc_type,
-                "bc_values": {"t": t.tolist(), bc_val: pa.tolist()},
-            },
-            {
-                "bc_name": "gnd",
-                "bc_type": "RCR",
-                "bc_values": {"C": 0.0, "Pd": 0.0, "Rd": 0.0, "Rp": 0.0},
-            },
-        ],
-        "simulation_parameters": {
-            "number_of_cardiac_cycles": 200,
-            "number_of_time_pts_per_cardiac_cycle": 1001,
-            "output_all_cycles": False,
-            "output_derivative": True,
-            "output_variable_based": True,
-        },
-        "vessels": [
-            {
-                "boundary_conditions": {"inlet": "pa", "outlet": "gnd"},
-                "vessel_id": 0,
-                "vessel_length": 1.0,
-                "vessel_name": "myocardium",
-                "zero_d_element_type": "BloodVessel",
-                "zero_d_element_values": {
-                    "C": 0.0,
-                    "L": 0.0,
-                    "R_poiseuille": 0.0,
-                    "stenosis_coefficient": 0.0,
-                },
-            }
-        ],
-    }
+    with open("myocardium.json", "r") as f:
+        forward = json.load(f)
+    forward["boundary_conditions"][0]["bc_type"] = bc_type
+    forward["boundary_conditions"][0]["bc_values"]["t"] = t.tolist()
+    forward["boundary_conditions"][0]["bc_values"][bc_val] = pa.tolist()
     return forward
 
 
@@ -92,45 +62,33 @@ def create_inverse_config(forward, res_forward):
     return inverse
 
 
-def optimize_zero_d(config, reference):
-    param_keys = ["R_poiseuille", "stenosis_coefficient"] # "C", "L", "stenosis_coefficient"]
-    # param_keys = list(config["vessels"][0]["zero_d_element_values"].keys())
-    p0 = [config["vessels"][0]["zero_d_element_values"][k] for k in param_keys]
+def get_vol(config):
+    sim = pysvzerod.simulate(config)
+    name = "flow:pa:RC1"
+    y = sim[sim["name"] == name]["y"].to_numpy()
+    tmax = config["boundary_conditions"][0]["bc_values"]["t"][-1]
+    nt = config["simulation_parameters"]["number_of_time_pts_per_cardiac_cycle"]
+    return np.cumsum(y) * tmax / nt
+
+def set_params(config, param_keys, p):
+    for i, (id, k) in enumerate(param_keys):
+        config["vessels"][id]["zero_d_element_values"][k] = np.exp(p[i])
+    return config
+
+def optimize_zero_d(config, vol_ref):
+    param_keys = [(0, "C"), (0, "R_poiseuille"), (1, "C"), (1, "R_poiseuille")]
 
     def cost_function(p):
-        for i, k in enumerate(param_keys):
-            config["vessels"][0]["zero_d_element_values"][k] = p[i]
-        sim = pysvzerod.simulate(config)
-        cost = []
-        for k in reference.keys():
-            for name, ref in reference[k].items():
-                y = sim[sim["name"] == name][k]
-                cost += [np.array(y) - np.array(ref)]
-        return np.array(cost).flatten()
+        set_params(config, param_keys, p)
+        print(np.exp(p))
+        cost = vol_ref - vol_ref[0] - get_vol(config)
+        print(np.linalg.norm(cost))
+        return cost
 
-    res = least_squares(
-        cost_function, np.ones(len(param_keys)), jac="2-point", method="lm"
-    )
-    for i, k in enumerate(param_keys):
-        config["vessels"][0]["zero_d_element_values"][k] = res.x[i]
-
-    # sim = pysvzerod.simulate(config)
-    # rname = "pressure:pa:myocardium"
-    # pa = sim[sim["name"] == rname]["y"]
-    # t = sim[sim["name"] == rname]["time"]
-    # fig, ax1 = plt.subplots()
-    # ax2 = ax1.twinx()
-    # ax1.plot(t, reference["y"][rname], "b", label="measured")
-    # ax1.plot(t, pa, "y", label="simulated")
-    # ax2.plot(
-    #     config["boundary_conditions"][0]["bc_values"]["t"],
-    #     config["boundary_conditions"][0]["bc_values"]["Q"],
-    #     "r",
-    #     label="inflow",
-    # )
-    # ax1.legend()
-    # ax2.legend()
-    # plt.show()
+    p0 = np.log(0.1*np.ones(len(param_keys)))
+    # pdb.set_trace()
+    res = least_squares(cost_function, p0, jac="2-point", method="lm")
+    set_params(config, param_keys, res.x)
     return config
 
 
@@ -154,18 +112,69 @@ def main_ideal():
     print(optimized_config["vessels"][0]["zero_d_element_values"])
 
 
+def estimate_rc(t, p, v):
+    p_smooth = savgol_filter(p, window_length=11, polyorder=2)
+    v_smooth = savgol_filter(v, window_length=11, polyorder=2)
+
+    dp = np.gradient(p_smooth, t)
+    dv = np.gradient(v_smooth, t)
+
+    pmax = p_smooth.max()
+    pmin = p_smooth.min()
+    dpmax = dp.max()
+    dpmin = dp.min()
+    dvmax = dv.max()
+    dvmin = dv.min()
+
+    c1 = - dvmax / dpmin
+    c2 = - dvmin / dpmax
+    r1 = (pmax - pmin) / dvmax
+    r2 = (pmin - pmax) / dvmin
+    # plt.plot(v, p, "k-")
+    # plt.plot(v_smooth, p_smooth, "ro")
+    # plt.show()
+    return [c1, r1, c2, r2]
+
 def estimate(fname):
     t, pa, pv, v, flow = read_data(fname)
-    forward = create_forward_config(t, flow, "q")
-    time_points = forward["simulation_parameters"][
-        "number_of_time_pts_per_cardiac_cycle"
-    ]
-    t_solve = np.linspace(t[0], t[-1], time_points)
-    pa_solve = np.interp(t_solve, t, pa)
-    reference = {"y": {"pressure:pa:myocardium": pa_solve.tolist()}}
+    # unit conversion
+    mmHg_to_CGS = 1.33322e3
+    pv *= mmHg_to_CGS
+    pa *= mmHg_to_CGS
 
-    optimized_config = optimize_zero_d(forward, reference)
+    # create 0D model
+    forward = create_forward_config(t, pv, "p")
+    nt = forward["simulation_parameters"]["number_of_time_pts_per_cardiac_cycle"]
+
+    # interoplate to the same time points
+    t_sim = np.linspace(t[0], t[-1], nt)
+    p_sim = np.interp(t_sim, t, pv)
+    v_sim = np.interp(t_sim, t, v)
+
+    p_estim = estimate_rc(t, pv, v)
+    param_keys = [(0, "C"), (0, "R_poiseuille"), (1, "C"), (1, "R_poiseuille")]
+    set_params(forward, param_keys, np.log(p_estim))
+    v_sim = get_vol(forward)
+    plt.plot(v - v[0], pv, "k-")
+    plt.plot(v_sim, p_sim, "ro")
+    plt.show()
+    pdb.set_trace()
+
+
+    optimized_config = optimize_zero_d(forward, v_sim)
     print(optimized_config["vessels"][0]["zero_d_element_values"])
+
+    v_sim = get_vol(optimized_config)
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    # ax1.plot(pv, v, "b", label="measured")
+    # ax1.plot(p_sim, v_sim, "y", label="simulated")
+    ax1.plot(t, pv, "r", label="pv")
+    ax2.plot(t_sim, v_sim, "r", label="pv")
+    ax1.legend()
+    # ax2.legend()
+    plt.show()
+    pdb.set_trace()
 
 
 def main():
