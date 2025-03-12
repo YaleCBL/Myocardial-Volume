554eb3a7f070aa9f1ec6d1e1a7a0af5c9c159c5a
Martin Pfaller <martin.pfaller@yale.edu>
2025-03-12 00:35:09 -0400

    estimates resistance for all cases

--- process.py
+++ process.py
@@ -10,6 +10,7 @@ import json
 from scipy.interpolate import interp1d
 from scipy.optimize import minimize, least_squares, Bounds
 
+
 def read_data(csv_file):
     df = pd.read_csv(csv_file)
     t = df["t abs [s]"].to_numpy()
@@ -19,13 +20,20 @@ def read_data(csv_file):
     flow = df["LAD Flow [mL/s]"].to_numpy()
     return t, pa, pv, v, flow
 
-def create_forward_config(t, pa):
+
+def create_forward_config(t, pa, intype):
+    if intype == "p":
+        bc_type = "PRESSURE"
+        bc_val = "P"
+    elif intype == "q":
+        bc_type = "FLOW"
+        bc_val = "Q"
     forward = {
         "boundary_conditions": [
             {
                 "bc_name": "pa",
-                "bc_type": "PRESSURE",
-                "bc_values": {"t": t.tolist(), "P": pa.tolist()},
+                "bc_type": bc_type,
+                "bc_values": {"t": t.tolist(), bc_val: pa.tolist()},
             },
             {
                 "bc_name": "gnd",
@@ -34,7 +42,7 @@ def create_forward_config(t, pa):
             },
         ],
         "simulation_parameters": {
-            "number_of_cardiac_cycles": 10,
+            "number_of_cardiac_cycles": 200,
             "number_of_time_pts_per_cardiac_cycle": 1001,
             "output_all_cycles": False,
             "output_derivative": True,
@@ -48,15 +56,16 @@ def create_forward_config(t, pa):
                 "vessel_name": "myocardium",
                 "zero_d_element_type": "BloodVessel",
                 "zero_d_element_values": {
-                    "C": 0.0001,
-                    "L": 1.0,
-                    "R_poiseuille": 100.0,
+                    "C": 0.0,
+                    "L": 0.0,
+                    "R_poiseuille": 0.0,
                     "stenosis_coefficient": 0.0,
                 },
             }
         ],
     }
     return forward
+
 
 def create_inverse_config(forward, res_forward):
     inverse = copy.deepcopy(forward)
@@ -82,8 +91,9 @@ def create_inverse_config(forward, res_forward):
             inverse["dy"][key] = res["ydot"].tolist()
     return inverse
 
+
 def optimize_zero_d(config, reference):
-    param_keys = ['C', 'L', 'R_poiseuille']
+    param_keys = ["R_poiseuille", "stenosis_coefficient"] # "C", "L", "stenosis_coefficient"]
     # param_keys = list(config["vessels"][0]["zero_d_element_values"].keys())
     p0 = [config["vessels"][0]["zero_d_element_values"][k] for k in param_keys]
 
@@ -92,32 +102,47 @@ def optimize_zero_d(config, reference):
             config["vessels"][0]["zero_d_element_values"][k] = p[i]
         sim = pysvzerod.simulate(config)
         cost = []
-        for k in ["y", "ydot"]:
+        for k in reference.keys():
             for name, ref in reference[k].items():
                 y = sim[sim["name"] == name][k]
                 cost += [np.array(y) - np.array(ref)]
         return np.array(cost).flatten()
 
-    res = least_squares(cost_function, np.ones(len(param_keys)), jac='2-point', method="lm")
+    res = least_squares(
+        cost_function, np.ones(len(param_keys)), jac="2-point", method="lm"
+    )
     for i, k in enumerate(param_keys):
         config["vessels"][0]["zero_d_element_values"][k] = res.x[i]
+
+    # sim = pysvzerod.simulate(config)
+    # rname = "pressure:pa:myocardium"
+    # pa = sim[sim["name"] == rname]["y"]
+    # t = sim[sim["name"] == rname]["time"]
+    # fig, ax1 = plt.subplots()
+    # ax2 = ax1.twinx()
+    # ax1.plot(t, reference["y"][rname], "b", label="measured")
+    # ax1.plot(t, pa, "y", label="simulated")
+    # ax2.plot(
+    #     config["boundary_conditions"][0]["bc_values"]["t"],
+    #     config["boundary_conditions"][0]["bc_values"]["Q"],
+    #     "r",
+    #     label="inflow",
+    # )
+    # ax1.legend()
+    # ax2.legend()
+    # plt.show()
     return config
 
-def main():
+
+def main_ideal():
     t, pa, pv, v, flow = read_data("DSEA08_baseline.csv")
-    forward = create_forward_config(t, pa)
+    forward = create_forward_config(t, pa, "p")
     with open("forward.json", "w") as f:
         json.dump(forward, f, indent=2)
     res_forward = pysvzerod.simulate(forward)
 
-    inverse = create_inverse_config(forward, res_forward)
-    with open("inverse.json", "w") as f:
-        json.dump(inverse, f, indent=2)
-    res_inverse = pysvzerod.calibrate(inverse)
-    print(res_inverse["vessels"][0]["zero_d_element_values"])
-
     reference = {"y": {}, "ydot": {}}
-    names = ["pa:myocardium", "myocardium:gnd"]
+    names = ["pa:myocardium"]  # , "myocardium:gnd"]
     for n in names:
         for f in ["flow", "pressure"]:
             key = f + ":" + n
@@ -127,6 +152,33 @@ def main():
 
     optimized_config = optimize_zero_d(forward, reference)
     print(optimized_config["vessels"][0]["zero_d_element_values"])
+
+
+def estimate(fname):
+    t, pa, pv, v, flow = read_data(fname)
+    forward = create_forward_config(t, flow, "q")
+    time_points = forward["simulation_parameters"][
+        "number_of_time_pts_per_cardiac_cycle"
+    ]
+    t_solve = np.linspace(t[0], t[-1], time_points)
+    pa_solve = np.interp(t_solve, t, pa)
+    reference = {"y": {"pressure:pa:myocardium": pa_solve.tolist()}}
+
+    optimized_config = optimize_zero_d(forward, reference)
+    print(optimized_config["vessels"][0]["zero_d_element_values"])
+
+
+def main():
+    studies = [
+        "DSEA08_baseline.csv",
+        "DSEA08_mild_sten.csv",
+        "DSEA08_mild_sten_dob.csv",
+        "DSEA08_mod_sten.csv",
+        "DSEA08_mod_sten_dob.csv",
+    ]
+    for study in studies:
+        estimate(study)
+
 
 if __name__ == "__main__":
     main()
